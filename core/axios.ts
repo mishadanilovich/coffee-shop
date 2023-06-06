1b247ae85fbfbad306acaacb812dd9697d1526a1
PinkyPie <danilovich36misha@gmail.com>
2023-06-06 09:58:38 +0200

    Fix Cors

--- core/axios.ts
+++ core/axios.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 import { parseCookies } from 'nookies';
 
 axios.defaults.baseURL = 'https://coffee-shop-doppio.vercel.app';
-axios.defaults.withCredentials = true;
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 axios.interceptors.request.use(config => {
 	if (typeof window !== 'undefined') {
