7092cc340bedec1e7047576284ad938cfca24826
PinkyPie <danilovich36misha@gmail.com>
2023-06-09 13:35:24 +0200

    Add new env variable

--- core/axios.ts
+++ core/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { parseCookies } from 'nookies';
 
-axios.defaults.baseURL = 'https://coffee-shop-doppio.vercel.app';
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:7777';
 
 axios.interceptors.request.use(config => {
 	if (typeof window !== 'undefined') {
