fbc88332b5b149f024de1dc02df0088340ca0e94
PinkyPie <danilovich36misha@gmail.com>
2023-06-14 16:05:14 +0200

    Add fallback image

--- src/components/constants/index.ts
+++ src/components/constants/index.ts
@@ -3,7 +3,7 @@ export const CURRENCY = 'Br';
 export const ERROR_TITLE = 'Oops!';
 export const ERROR_MESSAGE = 'Something went wrong...';
 
-export const FALLBACK_MENU_IMAGE_SRC = '/mask-menu-image.jpg';
+export const FALLBACK_MENU_IMAGE_SRC = '/fallback-menu-image.jpg';
 
 export const COFFEE_SHOP_COORDS = { lat: 53.67793240751909, lng: 23.826555466952357 };
 
